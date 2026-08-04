be47637288b76b7122c9275e898543ab0ec22304
jciado-eng <jhonnyciado@gmail.com>
2026-08-04 16:19:38 +0200

    Refactor CartItem component and update handlers

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -11,11 +11,13 @@ const CartItem = ({ onContinueShopping }) => {
     return cart.reduce((total, item) => total + (parseFloat(item.cost.substring(1)) * item.quantity), 0);
   };
 
-  const handleContinueShopping = (e) => onContinueShopping(e);
+  const calculateTotalCost = (item) => {
+    return (parseFloat(item.cost.substring(1)) * item.quantity).toFixed(2);
+  };
 
-  const handleCheckoutShopping = (e) => alert('Functionality to be added for future reference');
-
-  const handleIncrement = (item) => dispatch(updateQuantity({ ...item, quantity: item.quantity + 1 }));
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ ...item, quantity: item.quantity + 1 }));
+  };
 
   const handleDecrement = (item) => {
     if (item.quantity > 1) {
@@ -25,9 +27,9 @@ const CartItem = ({ onContinueShopping }) => {
     }
   };
 
-  const handleRemove = (item) => dispatch(removeItem(item.name));
-
-  const calculateTotalCost = (item) => parseFloat(item.cost.substring(1)) * item.quantity;
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+  };
 
   return (
     <div className="cart-container">
@@ -40,19 +42,19 @@ const CartItem = ({ onContinueShopping }) => {
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button className="cart-item-button" onClick={() => handleDecrement(item)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button className="cart-item-button" onClick={() => handleIncrement(item)}>+</button>
               </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item).toFixed(2)}</div>
+              <div className="cart-item-total">Subtotal: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
             </div>
           </div>
         ))}
       </div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
-        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
+        <button className="get-started-button" onClick={onContinueShopping}>Continue Shopping</button>
+        <button className="get-started-button1" onClick={() => alert('Checkout in progress...')}>Checkout</button>
       </div>
     </div>
   );
