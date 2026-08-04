f2917676e63dc463738362ea3edb81d709c3b8f3
jciado-eng <jhonnyciado@gmail.com>
2026-08-04 16:17:18 +0200

    Update ProductList to use useSelector for cart items
    Refactor ProductList component to use useSelector for cart items and calculate total quantity.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,23 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import React, { useState } from 'react';
+import './ProductList.css';
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false);
     const [addedToCart, setAddedToCart] = useState({});
 
+    const CartItems = useSelector(state => state.cart.items);
+
+    const calculateTotalQuantity = () => { 
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0; 
+    };
+
     const handleAddToCart = (product) => {
-        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+        dispatch(addItem(product)); // dispatch(addItem(product))
       
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        setAddedToCart((prevState) => ({
+          ...prevState,
+          [product.name]: true,
         }));
     };
+
 
     const plantsArray = [
         {
